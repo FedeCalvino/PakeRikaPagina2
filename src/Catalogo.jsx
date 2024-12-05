49c9442c2222355776dfb5b8738804896ba3fa67
Fitzroyfang <Calvinofede@gmail.com>
2024-12-05 11:52:54 -0300

    vercel

--- src/Catalogo.jsx
+++ src/Catalogo.jsx
@@ -39,7 +39,7 @@ export const Catalogo = () => {
 
   const fetchArticulos = async () => {
     try {
-      const response = await fetch(`http://200.40.89.254:3034/articulosAll`);
+      const response = await fetch(`/articulos`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
