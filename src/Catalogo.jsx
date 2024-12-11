502c4d6fd84cdcce759252e60f4582f1fd2e27e2
Fitzroyfang <Calvinofede@gmail.com>
2024-12-10 23:39:20 -0300

    empanadas numero

--- src/Catalogo.jsx
+++ src/Catalogo.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import './Catalogo.css';
-import { HeaderPakeRika } from './HeaderPakeRika';
-import { Carrito } from './Carrito';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCarritoSlice } from './Features/CarritoSlice';
-import { updateProducto, updateTotal } from './Features/CarritoSlice';
+import React, { useState, useEffect } from "react";
+import "./Catalogo.css";
+import { HeaderPakeRika } from "./HeaderPakeRika";
+import { Carrito } from "./Carrito";
+import { useDispatch, useSelector } from "react-redux";
+import { setCarritoSlice } from "./Features/CarritoSlice";
+import { updateProducto, updateTotal } from "./Features/CarritoSlice";
 export const Catalogo = () => {
-
   const [Artiuclos, setArticulos] = useState([]);
   const [highlightedId, setHighlightedId] = useState(null);
   const [highlightedIdR, setHighlightedIdR] = useState(null);
@@ -19,51 +18,58 @@ export const Catalogo = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const CarritoGuardado = localStorage.getItem('carrito');
+    const CarritoGuardado = localStorage.getItem("carrito");
 
-    console.log("CarritoGuardado", CarritoGuardado)
+    console.log("CarritoGuardado", CarritoGuardado);
 
     if (CarritoGuardado) {
-      const carritoObjeto = JSON.parse(CarritoGuardado)
-      console.log("carritoObjeto", carritoObjeto)
-      dispatch(setCarritoSlice(carritoObjeto))
+      const carritoObjeto = JSON.parse(CarritoGuardado);
+      console.log("carritoObjeto", carritoObjeto);
+      dispatch(setCarritoSlice(carritoObjeto));
     }
 
     fetchArticulos();
   }, []);
 
   useEffect(() => {
-    console.log("carrito gu")
-    localStorage.setItem('carrito', JSON.stringify(carrito));
+    console.log("carrito gu");
+    localStorage.setItem("carrito", JSON.stringify(carrito));
   }, [carrito]);
 
   const fetchArticulos = async () => {
     try {
       const response = await fetch(`/articulos`);
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
       console.log("data", data);
-      setArticulos(data);  // Corrected typo
+      setArticulos(data); // Corrected typo
     } catch (error) {
-      console.error('There has been a problem with your fetch operation:', error);
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
     }
   };
 
   const toggleCarrito = () => {
-    console.log(carrito)
-    setCarritoVisible(prev => !prev);
+    console.log(carrito);
+    setCarritoVisible((prev) => !prev);
   };
 
   const updateCarrito = (empanada, quantityChange) => {
-    console.log("Actualizando carrito con empanada:", empanada, "Cantidad:", quantityChange);
-    console.log("carrito", carrito)
+    console.log(
+      "Actualizando carrito con empanada:",
+      empanada,
+      "Cantidad:",
+      quantityChange
+    );
+    console.log("carrito", carrito);
     dispatch(updateProducto({ empanada, quantityChange }));
     dispatch(updateTotal({ empanada, quantityChange }));
-    localStorage.setItem('carrito', JSON.stringify(carrito));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
   };
-
 
   const addArticulo = (empanada) => {
     setHighlightedId(empanada._id);
@@ -78,10 +84,11 @@ export const Catalogo = () => {
   };
 
   const getCantidadEmpanada = (id) => {
-    const empanadaInCarrito = carrito.Carrito.prods ? carrito.Carrito.prods.find(item => item._id === id) : null;
+    const empanadaInCarrito = carrito.Carrito.prods
+      ? carrito.Carrito.prods.find((item) => item._id === id)
+      : null;
     return empanadaInCarrito ? empanadaInCarrito.cantidad : 0;
   };
-
 
   return (
     <>
@@ -95,17 +102,6 @@ export const Catalogo = () => {
             textAlign: "center",
           }}
         >
-          <img
-            style={{
-              width: "50%",
-              maxWidth: "200px",
-              height: "auto",
-              userSelect: "none",
-              display: "block",
-            }}
-            src="empanada-removebg.png"
-            alt={"Empanada"}
-          />
           <p
             style={{
               fontSize: "40px",
@@ -119,124 +115,160 @@ export const Catalogo = () => {
           </p>
         </div>
 
-        <div className={carritoVisible ? "grid-container" : "grid-containerFull"}>
-          {Artiuclos.map(Artiuclo => (
-            Artiuclo.Categoria === "Empanadas" &&
-            <div className={`card ${highlightedId === Artiuclo._id && 'highlight'} ${highlightedIdR === Artiuclo._id && 'highlightR'}`} key={Artiuclo._id}>
-              {getCantidadEmpanada(Artiuclo._id) > 0 && (
-                <>
-                  <img
-                    className='remove-btn'
-                    src="./borrar-removebg.png"
-                    alt="Eliminar"
-                    style={{ userSelect: "none" }}
-                    onClick={() => removeEmpanada(Artiuclo)}
-                  />
-                  <div className="quantity-badge" style={{ userSelect: "none" }}>
-                    {getCantidadEmpanada(Artiuclo._id)}
-                  </div>
-                </>
-              )}
-              <div style={{padding: "5px"}} className='cartaAdd' onClick={() => addArticulo(Artiuclo) }>
-                <h2>{Artiuclo.Nombre}</h2>
-                <h2>{Artiuclo.Numero}</h2>
+        <div
+          className={carritoVisible ? "grid-container" : "grid-containerFull"}
+        >
+          {Artiuclos.filter((Artiuclo) => Artiuclo.Categoria === "Empanadas")
+            .sort((a, b) => a.Numero - b.Numero)
+            .map((Artiuclo) => (
+              <div
+                className={`card ${
+                  highlightedId === Artiuclo._id && "highlight"
+                } ${highlightedIdR === Artiuclo._id && "highlightR"}`}
+                key={Artiuclo._id}
+              >
+                {getCantidadEmpanada(Artiuclo._id) > 0 && (
+                  <>
+                    <img
+                      className="remove-btn"
+                      src="./borrar-removebg.png"
+                      alt="Eliminar"
+                      style={{ userSelect: "none" }}
+                      onClick={() => removeEmpanada(Artiuclo)}
+                    />
+                    <div
+                      className="quantity-badge"
+                      style={{ userSelect: "none" }}
+                    >
+                      {getCantidadEmpanada(Artiuclo._id)}
+                    </div>
+                  </>
+                )}
+                <div
+                  style={{ padding: "5px" }}
+                  className="cartaAdd"
+                  onClick={() => addArticulo(Artiuclo)}
+                >
+                  <h2>{Artiuclo.Nombre}</h2>
+                  <h2>{Artiuclo.Numero}</h2>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
       <div
+        style={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center", // Centra verticalmente
+          textAlign: "center",
+        }}
+      >
+        <p
           style={{
-            width: "100%",
-            display: "flex",
-            alignItems: "center", // Centra verticalmente
+            fontSize: "40px",
             textAlign: "center",
+            marginLeft: "150px",
+            fontWeight: "bold",
+            color: "#333",
+            margin: "0 10px", // Espaciado entre imagen y texto
           }}
         >
-          <p
-            style={{
-              fontSize: "40px",
-              textAlign: "center",
-              marginLeft:"150px",
-              fontWeight: "bold",
-              color: "#333",
-              margin: "0 10px", // Espaciado entre imagen y texto
-            }}
-          >
-            Promos
-          </p>
-        </div>
+          Promos
+        </p>
+      </div>
       <div className={carritoVisible ? "grid-container" : "grid-containerFull"}>
-          {Artiuclos.map(Artiuclo => (
-            Artiuclo.Categoria === "Promo" &&
-            <div className={`card ${highlightedId === Artiuclo._id && 'highlight'} ${highlightedIdR === Artiuclo._id && 'highlightR'}`} key={Artiuclo._id}>
-              {getCantidadEmpanada(Artiuclo._id) > 0 && (
-                <>
-                  <img
-                    className='remove-btn'
-                    src="./borrar-removebg.png"
-                    alt="Eliminar"
-                    style={{ userSelect: "none" }}
-                    onClick={() => removeEmpanada(Artiuclo)}
-                  />
-                  <div className="quantity-badge" style={{ userSelect: "none" }}>
-                    {getCantidadEmpanada(Artiuclo._id)}
-                  </div>
-                </>
-              )}
-              <div className='cartaAdd' onClick={() => addArticulo(Artiuclo)}>
-                <h2>{Artiuclo.Nombre}</h2>
+        {Artiuclos.map(
+          (Artiuclo) =>
+            Artiuclo.Categoria === "Promo" && (
+              <div
+                className={`card ${
+                  highlightedId === Artiuclo._id && "highlight"
+                } ${highlightedIdR === Artiuclo._id && "highlightR"}`}
+                key={Artiuclo._id}
+              >
+                {getCantidadEmpanada(Artiuclo._id) > 0 && (
+                  <>
+                    <img
+                      className="remove-btn"
+                      src="./borrar-removebg.png"
+                      alt="Eliminar"
+                      style={{ userSelect: "none" }}
+                      onClick={() => removeEmpanada(Artiuclo)}
+                    />
+                    <div
+                      className="quantity-badge"
+                      style={{ userSelect: "none" }}
+                    >
+                      {getCantidadEmpanada(Artiuclo._id)}
+                    </div>
+                  </>
+                )}
+                <div className="cartaAdd" onClick={() => addArticulo(Artiuclo)}>
+                  <h2>{Artiuclo.Nombre}</h2>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div
+            )
+        )}
+      </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center", // Centra verticalmente
+          textAlign: "center",
+        }}
+      >
+        <p
           style={{
-            width: "100%",
-            display: "flex",
-            alignItems: "center", // Centra verticalmente
+            fontSize: "40px",
             textAlign: "center",
+            marginLeft: "150px",
+            fontWeight: "bold",
+            color: "#333",
+            margin: "0 10px", // Espaciado entre imagen y texto
           }}
         >
-          <p
-            style={{
-              fontSize: "40px",
-              textAlign: "center",
-              marginLeft:"150px",
-              fontWeight: "bold",
-              color: "#333",
-              margin: "0 10px", // Espaciado entre imagen y texto
-            }}
-          >
-            Bebidas
-          </p>
-        </div>
+          Bebidas
+        </p>
+      </div>
       <div className={carritoVisible ? "grid-container" : "grid-containerFull"}>
-          {Artiuclos.map(Artiuclo => (
-            (Artiuclo.Categoria === "Bebida" || Artiuclo.Categoria === "Agua") &&
-            <div className={`card ${highlightedId === Artiuclo._id && 'highlight'} ${highlightedIdR === Artiuclo._id && 'highlightR'}`} key={Artiuclo._id}>
-              {getCantidadEmpanada(Artiuclo._id) > 0 && (
-                <>
-                  <img
-                    className='remove-btn'
-                    src="./borrar-removebg.png"
-                    alt="Eliminar"
-                    style={{ userSelect: "none" }}
-                    onClick={() => removeEmpanada(Artiuclo)}
-                  />
-                  <div className="quantity-badge" style={{ userSelect: "none" }}>
-                    {getCantidadEmpanada(Artiuclo._id)}
-                  </div>
-                </>
-              )}
-              <div className='cartaAdd' onClick={() => addArticulo(Artiuclo)}>
-                <h2>{Artiuclo.Nombre}</h2>
+        {Artiuclos.map(
+          (Artiuclo) =>
+            (Artiuclo.Categoria === "Bebida" ||
+              Artiuclo.Categoria === "Agua") && (
+              <div
+                className={`card ${
+                  highlightedId === Artiuclo._id && "highlight"
+                } ${highlightedIdR === Artiuclo._id && "highlightR"}`}
+                key={Artiuclo._id}
+              >
+                {getCantidadEmpanada(Artiuclo._id) > 0 && (
+                  <>
+                    <img
+                      className="remove-btn"
+                      src="./borrar-removebg.png"
+                      alt="Eliminar"
+                      style={{ userSelect: "none" }}
+                      onClick={() => removeEmpanada(Artiuclo)}
+                    />
+                    <div
+                      className="quantity-badge"
+                      style={{ userSelect: "none" }}
+                    >
+                      {getCantidadEmpanada(Artiuclo._id)}
+                    </div>
+                  </>
+                )}
+                <div className="cartaAdd" onClick={() => addArticulo(Artiuclo)}>
+                  <h2>{Artiuclo.Nombre}</h2>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            )
+        )}
+      </div>
       {carritoVisible && (
-        <div className='carritoDiv'>
+        <div className="carritoDiv">
           <Carrito />
         </div>
       )}
