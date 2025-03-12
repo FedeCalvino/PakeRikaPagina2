751ff3207b0d304f5d4b3e61e8959fc439084400
Fitzroyfang <Calvinofede@gmail.com>
2025-03-12 12:55:58 -0300

    datos todo el dia

--- src/Datos.jsx
+++ src/Datos.jsx
@@ -12,13 +12,20 @@ import {
 import { HeaderPakeRika } from "./HeaderPakeRika";
 
 // Registrar los componentes de Chart.js
-ChartJS.register(BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const Datos = () => {
   const [ordenes, setOrdenes] = useState([]);
 
   const [Local, setLocal] = useState(() => {
-    const storedLocal = sessionStorage.getItem('local');
+    const storedLocal = sessionStorage.getItem("local");
     return storedLocal ? JSON.parse(storedLocal) : null;
   });
 
@@ -27,12 +34,14 @@ export const Datos = () => {
     const localDate = new Date(date.getTime() - offset * 60 * 1000); // Ajusta la fecha al huso local
     return localDate.toISOString().split("T")[0]; // Devuelve solo la parte de la fecha
   };
-  
+
   const [fechaFiltro, setFechaFiltro] = useState(getLocalISOString(new Date()));
 
   const fetchOrdenes = async () => {
     try {
-      const response = await fetch(`/OrdenesLocal?local=${Local}`);
+      const response = await fetch(
+        `/OrdenesLocal?local=${Local}`
+      );
       const data = await response.json();
       setOrdenes(data);
     } catch (error) {
@@ -79,8 +88,6 @@ export const Datos = () => {
       ],
     };
   };
-  
-  
 
   const calcularTotalesPorRango = (inicio, fin) => {
     return ordenes.reduce((total, orden) => {
@@ -102,8 +109,30 @@ export const Datos = () => {
     }, 0);
   };
 
-  const total11a16 = calcularTotalesPorRango(11, 16);
+  const calcularTotalesEfectivoPorRango = (inicio, fin) => {
+    return ordenes.reduce((total, orden) => {
+      const fechaOrden = new Date(orden.Dia);
+      const fechaSeleccionada = new Date(fechaFiltro);
+      fechaSeleccionada.setDate(fechaSeleccionada.getDate() + 1);
+
+      if (
+        fechaOrden.getFullYear() === fechaSeleccionada.getFullYear() &&
+        fechaOrden.getMonth() === fechaSeleccionada.getMonth() &&
+        fechaOrden.getDate() === fechaSeleccionada.getDate()
+      ) {
+        const hora = fechaOrden.getHours();
+        if (hora >= inicio && hora <= fin && orden.Pago === "Efectivo") {
+          return total + orden.Total;
+        }
+      }
+      return total;
+    }, 0);
+  };
+
+  const total7a21 = calcularTotalesPorRango(7, 22);
+  const totalEfectivo7a21 = calcularTotalesEfectivoPorRango(7, 22);
   const total20a24 = calcularTotalesPorRango(20, 23);
+  const totalEfectivo20a24 = calcularTotalesEfectivoPorRango(20, 23);
 
   return (
     <>
@@ -116,6 +145,7 @@ export const Datos = () => {
             alignItems: "center",
             gap: "10px",
             marginBottom: "20px",
+            maxHeight: "500px",
           }}
         >
           <button
@@ -142,10 +172,28 @@ export const Datos = () => {
             &#8594;
           </button>
         </div>
-        <div style={{ display: "flex", gap: "20px" }}>
-          <div style={{ flex: "1", padding: "10px", border: "1px solid #ddd" }}>
+        <div
+          style={{
+            display: "flex",
+            gap: "20px",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              flex: "1",
+              padding: "10px",
+              border: "1px solid #ddd",
+              height: "500px",
+              width: "100%",
+              display:"flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <Bar
-              data={procesarDatosPorRango(11, 16)}
+              data={procesarDatosPorRango(7, 21)}
               options={{
                 responsive: true,
                 plugins: {
@@ -153,69 +201,44 @@ export const Datos = () => {
                   title: { display: true, text: "Órdenes de 12:00 a 16:00" },
                 },
                 ticks: {
-                    stepSize: 1, // Incrementa el eje Y de 1 en 1
-                  }
-              }}
-            />
-           <div
-          style={{
-            border: "1px solid #ddd",
-            padding: "20px",
-            marginTop: "20px",
-            borderRadius: "12px",
-            backgroundColor: "#e8f5e9",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-            textAlign: "center",
-          }}
-        >
-          <p
-            style={{
-              fontSize: "2rem",
-              color: "#2e7d32",
-              fontWeight: "bold",
-              margin: "0",
-            }}
-          >
-            Total : <span style={{ fontSize: "2.5rem" }}>{total11a16}</span>
-          </p>
-        </div>
-          </div>
-          <div style={{ flex: "1", padding: "10px", border: "1px solid #ddd" }}>
-            <Bar
-              data={procesarDatosPorRango(20, 24)}
-              options={{
-                responsive: true,
-                plugins: {
-                  legend: { display: true, position: "top" },
-                  title: { display: true, text: "Órdenes de 20:00 a 24:00" },
+                  stepSize: 1, // Incrementa el eje Y de 1 en 1
                 },
-                ticks: {
-                    stepSize: 1, // Incrementa el eje Y de 1 en 1
-                }
               }}
             />
             <div
-          style={{
-            border: "1px solid #ddd",
-            padding: "20px",
-            marginTop: "20px",
-            borderRadius: "12px",
-            backgroundColor: "#e8f5e9",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-            textAlign: "center",
-          }}
-        >
-          <p
-            style={{
-              fontSize: "2rem",
-              color: "#2e7d32",
-              fontWeight: "bold",
-              margin: "0",
-            }}
-          >
-            Total : <span style={{ fontSize: "2.5rem" }}>{total20a24}</span>
-          </p>
-        </div>
+              style={{
+                border: "1px solid #ddd",
+                padding: "20px",
+                marginTop: "20px",
+                borderRadius: "12px",
+                backgroundColor: "#e8f5e9",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                textAlign: "center",
+                marginLeft:"20px"
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "2rem",
+                  color: "#2e7d32",
+                  fontWeight: "bold",
+                  margin: "0",
+                }}
+              >
+                Total : <span style={{ fontSize: "2.5rem" }}>{total7a21.toFixed()}$</span>
+              </p>
+              <p
+                style={{
+                  fontSize: "2rem",
+                  color: "#2e7d32",
+                  fontWeight: "bold",
+                  margin: "0",
+                }}
+              >
+                Efectivo :{" "}
+                <span style={{ fontSize: "2.5rem" }}>{totalEfectivo7a21.toFixed()}$</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
